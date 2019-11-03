a2c950e2aa119a733bf7dd40723a07cac373c212
wangx <wangxin@136.com>
2019-11-03 22:17:20 +0800

    用户信息修改

--- src/pages/adduser/adduser.jsx
+++ src/pages/adduser/adduser.jsx
@@ -8,10 +8,27 @@ const { Panel } = Collapse;
 function onChange(e) {
     console.log(`checked = ${e.target.checked}`);
 }
+
 class adduser extends Component {
 
+    onInputChange = (e) => {
+        let inputValue = e.target.value,
+            inputName = e.target.name
+        this.setState({
+            [inputName]: inputValue
+        })
+        // console.log(this.state)
+    }
+    getInputValue = () => {
+        console.log(this.state)
+    }
     state = {
-
+        Cname: '',
+        Ename: '',
+        HZH: '',
+        userName: '',
+        tel: '',
+        email: '',
     }
     render() {
         return <div className='adduser'>
@@ -35,26 +52,22 @@ class adduser extends Component {
                                 <p>顾客姓名</p>
                                 <form action="" >
                                     <label htmlFor="">中文名:</label>
-                                    <input type="text" placeholder="请输入中文名" />
+                                    <input type="text" placeholder="请输入中文名" name='Cname' onChange={e => this.onInputChange(e)} />
                                 </form><form action="" >
                                     <label htmlFor="">英文名:</label>
-                                    <input type="text" placeholder="请输入英文名" />
-                                </form>
-                                <form action="" >
-                                    <label htmlFor="">英文姓:</label>
-                                    <input type="text" placeholder="请输入英文姓" />
+                                    <input type="text" placeholder="请输入英文名" name='Ename' onChange={(e) => this.onInputChange(e)} />
                                 </form>
                                 <p>证件信息</p>
                                 <form action="" >
                                     <label htmlFor="">证件类型:</label>
-                                    <input type="text" placeholder="护照" />
+                                    <input type="text" placeholder="护照" disabled />
                                 </form><form action="" >
                                     <label htmlFor="">护照号:</label>
-                                    <input type="text" placeholder="请输入用户名" />
+                                    <input type="text" placeholder="护照号" name='HZH' onChange={(e) => this.onInputChange(e)} />
                                 </form>
                                 <form action="" >
                                     <label htmlFor="">国籍:</label>
-                                    <input type="text" placeholder="中国" />
+                                    <input type="text" placeholder="中国" disabled />
                                 </form>
                             </div>
 
@@ -64,14 +77,14 @@ class adduser extends Component {
                         <p>联系人信息</p>
                         <form action="" >
                             <label htmlFor="">联系人:</label>
-                            <input type="text" placeholder="请输入联系人姓名" />
+                            <input type="text" placeholder="请输入联系人姓名" name='userName' onChange={(e) => this.onInputChange(e)} />
                         </form><form action="" >
                             <label htmlFor="">手机号码:</label>
-                            <input type="text" placeholder="必填，用于接收信息" />
+                            <input type="text" placeholder="必填，用于接收信息" name='tel' onChange={(e) => this.onInputChange(e)} />
                         </form>
                         <form action="" >
                             <label htmlFor="">邮箱:</label>
-                            <input type="text" placeholder="必填，用于接收电子客票" />
+                            <input type="text" placeholder="必填，用于接收电子客票" name='email' onChange={(e) => this.onInputChange(e)} />
                         </form>
                     </div>
                     <div className='check'>
@@ -81,9 +94,9 @@ class adduser extends Component {
                         <a href="">《旅行合同等内容》</a>
                     </div>
                 </Content>
-                <Footer style={{ background: '#fff ' }}>
-                    <div className='price'>￥1000</div>
-                    <div className='next'>下一步</div>
+                <Footer style={{ background: '#fff ', padding: '12px' }}>
+                    <div className='price' style={{ float: "left", lineHeight: '48px' }}>￥1000</div>
+                    <div className='next' onClick={e => this.getInputValue(e)}> 下一步</div>
                 </Footer>
             </Layout>
         </div >
